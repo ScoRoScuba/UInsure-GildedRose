cf22a7e1c50d5b9894d4a6f4d84ae955174fca64
ScoRoScuba <scott.roberts@virgin.net>
2024-02-01 20:29:41 +0000

    Added TestCase for SellIn and Sulfuras Hand of Ragnaros which doesnt change

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -6,17 +6,27 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
+    [TestCase("foo", 0, -1)]
+    [TestCase("Sulfuras, Hand of Ragnaros", 0, 0)]
+    public void Item_SellInShouldDecrease(string itemName, int initialSellin, int expectedSellIn)
+    {
+        var items = new List<Item> { new Item { Name = itemName, SellIn = initialSellin, Quality = 0 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+       
+        items[0].SellIn.Should().Be(expectedSellIn);
+    }
+
     [Test]
-    public void Item_SellInShouldDecrease()
+    public void Item_WhenZeroQuality_QualityuDoesNotChange()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
 
         app.UpdateQuality();
 
-        items[0].Name.Should().Be("foo");
-        items[0].SellIn.Should().Be(-1);
+        items[0].Quality.Should().Be(0);
     }
-
 
 }
