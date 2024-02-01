ff5e9176c658983bda0885defd381cd9ac454b18
ScoRoScuba <scott.roberts@virgin.net>
2024-02-01 20:51:23 +0000

    Added test for SellIn Passed, Quality should decrease twice as fast

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -7,7 +7,7 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [TestCase("foo", 0, -1)]
-    [TestCase("Sulfuras, Hand of Ragnaros", 0, 0)]
+    [TestCase("foo1", 2, 1)]
     public void Item_SellInShouldDecrease(string itemName, int initialSellin, int expectedSellIn)
     {
         var items = new List<Item> { new Item { Name = itemName, SellIn = initialSellin, Quality = 0 } };
@@ -29,15 +29,38 @@ public class GildedRoseTest
         items[0].Quality.Should().Be(0);
     }
 
-    [Test]
-    public void WHenItemQualityGreaterThan0_QualitySHouldDecrease()
+    [TestCase("foo", 1, 0)]
+    [TestCase("foo1", 2, 1)]
+    public void WHenItemSellInDateValid_QualityShouldDecrease(string itemName, int initialQuality, int expectedQuality)
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 1 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = 2, Quality = initialQuality } };
         var app = new GildedRose(items);
 
         app.UpdateQuality();
 
-        items[0].Quality.Should().Be(0);
+        items[0].Quality.Should().Be(expectedQuality);
     }
 
+    [TestCase("foo", 4, 2)]
+    [TestCase("foo1", 2, 0)]
+    public void WHenItemSellInDatePassed_QualityShouldDecreaseTwiceAsFast(string itemName, int initialQuality, int expectedQuality)
+    {
+        var items = new List<Item> { new Item { Name = itemName, SellIn = 0, Quality = initialQuality } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+
+    public void WhenItemSulfuras_DoesNotUpdate()
+    {
+        var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        items[0].SellIn.Should().Be(10);
+        items[0].Quality.Should().Be(10);
+    }
 }
