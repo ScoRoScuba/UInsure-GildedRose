475b933d90ff86c5c5ed61c88b457b24d6fe6caa
ScoRoScuba <scott.roberts@immersedltd.co.uk>
2024-02-02 10:10:58 +0000

    Pulled out logic for Backstage Pass

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -47,6 +47,39 @@ public class GildedRose
                 continue;
             }
 
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+
+                    if (item.SellIn < 11)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                }
+
+                item.SellIn = item.SellIn - 1;
+
+                if (item.SellIn < 0)
+                {
+                    item.Quality = item.Quality - item.Quality;
+                }
+                continue;
+                
+            }
+
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (item.Quality > 0)
